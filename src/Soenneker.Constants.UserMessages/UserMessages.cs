088bd3ccffa934bac0bee7da6fd9c2ff2a8f0c4f
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:14 -0500

    xml commenting

--- src/Soenneker.Constants.UserMessages/UserMessages.cs
+++ src/Soenneker.Constants.UserMessages/UserMessages.cs
@@ -5,12 +5,30 @@
 /// </summary>
 public static class UserMessages
 {
+    /// <summary>
+    /// The something went wrong title.
+    /// </summary>
     public const string SomethingWentWrongTitle = "Something went wrong";
+    /// <summary>
+    /// The something went wrong detail.
+    /// </summary>
     public const string SomethingWentWrongDetail = "We couldn't complete your request. Please try again.";
 
+    /// <summary>
+    /// The request canceled title.
+    /// </summary>
     public const string RequestCanceledTitle = "Request canceled";
+    /// <summary>
+    /// The request canceled detail.
+    /// </summary>
     public const string RequestCanceledDetail = "The request was canceled before it could complete.";
 
+    /// <summary>
+    /// The timeout title.
+    /// </summary>
     public const string TimeoutTitle = "Request timeout";
+    /// <summary>
+    /// The timeout detail.
+    /// </summary>
     public const string TimeoutDetail = "The request took too long to complete.";
 }
